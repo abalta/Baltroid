5bcfc4ed00db1c52e8b4f27bff1258a7b2a2b3d0
tunahanbozkurt <t.bozkurt5775@gmail.com>
2023-04-27 12:31:45 +0300

    PlaceMarksScreen added.

--- app/src/main/java/com/baltroid/presentation/screens/home/HomeDetailScreen.kt
+++ app/src/main/java/com/baltroid/presentation/screens/home/HomeDetailScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.baltroid.apps.R
@@ -59,7 +60,7 @@ fun HomeDetailScreen(
                 .align(Alignment.BottomCenter)
         ) {
             Text(
-                text = "#KGD",
+                text = stringResource(id = R.string.hashtag_kgd),
                 style = MaterialTheme.localTextStyles.hashTag,
                 modifier = Modifier.padding(start = MaterialTheme.localDimens.dp17)
             )
--- app/src/main/java/com/baltroid/ui/theme/Color.kt
+++ app/src/main/java/com/baltroid/ui/theme/Color.kt
@@ -13,13 +13,15 @@ val Black_alpha03 = Color(0x4D000000)
 val Black_alpha02 = Color(0x33000000)
 val White = Color(0xFFFFFFFF)
 val White_alpha09 = Color(0xE6FFFFFF)
-val White_alpha07 = Color(0xB3FFFFFF)
 val White_alpha08 = Color(0xCCFFFFFF)
+val White_alpha07 = Color(0xB3FFFFFF)
+val White_alpha06 = Color(0x99FFFFFF)
 val White_alpha05 = Color(0x80FFFFFF)
 val White_alpha04 = Color(0x66FFFFFF)
 val Purple = Color(0xFF8D1697)
 val Pink = Color(0xFFED116D)
 val Orange = Color(0xFFFF7A00)
+val Yellow = Color(0xFFB39912)
 
 data class Colors(
     val black: Color = Black,
@@ -31,11 +33,13 @@ data class Colors(
     val white_alpha09: Color = White_alpha09,
     val white_alpha08: Color = White_alpha08,
     val white_alpha07: Color = White_alpha07,
+    val white_alpha06: Color = White_alpha06,
     val white_alpha04: Color = White_alpha04,
     val white_alpha05: Color = White_alpha05,
     val purple: Color = Purple,
     val pink: Color = Pink,
-    val orange: Color = Orange
+    val orange: Color = Orange,
+    val yellow: Color = Yellow,
 )
 
 val LocalColors = staticCompositionLocalOf { Colors() }
