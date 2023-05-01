d264189d560eacc9f34c3a830f5645fe028e9444
tunahanbozkurt <t.bozkurt5775@gmail.com>
2023-05-02 02:50:37 +0300

    optimizations

--- app/src/main/java/com/baltroid/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/baltroid/presentation/screens/home/HomeScreen.kt
@@ -1,18 +1,25 @@
 package com.baltroid.presentation.screens.home
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -21,19 +28,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import coil.compose.AsyncImage
@@ -54,6 +59,7 @@ fun HomeScreen(
     secondName: String,
     summary: String,
     genres: List<String>,
+    imgUrls: List<String>,
     numberOfNotification: Int,
     numberOfStory: Int,
     numberOfViews: Int,
@@ -64,13 +70,9 @@ fun HomeScreen(
 
     val lazyListState = rememberLazyListState()
     val scrollState = rememberScrollState()
-    val tabState by remember {
-        mutableStateOf(
-            HomeScreenTabState(
-                allStories = HomeScreenTabInfo(numberOfStory, true),
-                favorites = HomeScreenTabInfo(numberOfFavorites, false)
-            )
-        )
+
+    var tabState by rememberSaveable {
+        mutableStateOf(HomeScreenTabs.AllStories)
     }
 
     Column(
@@ -78,29 +80,35 @@ fun HomeScreen(
             .fillMaxSize()
             .verticalScroll(scrollState)
             .background(MaterialTheme.localColors.black)
+
     ) {
         HitReadsTopBar(
             iconResId = R.drawable.ic_bell_outlined,
             iconTint = MaterialTheme.localColors.white,
-            modifier = Modifier.padding(
-                start = MaterialTheme.localDimens.dp32,
-                end = MaterialTheme.localDimens.dp32,
-                top = MaterialTheme.localDimens.dp12,
-            ),
+            modifier = Modifier.statusBarsPadding(),
             numberOfNotification = numberOfNotification,
             onMenuCLicked = {},
             onNotificationClicked = {}
         )
         VerticalSpacer(height = MaterialTheme.localDimens.dp18_5)
         HomeScreenTabs(
-            modifier = Modifier.padding(start = MaterialTheme.localDimens.dp32),
-            tabState = tabState
-        ) {}
+            storiesSize = numberOfStory,
+            favoritesSize = numberOfFavorites,
+            selectedTab = tabState,
+            modifier = Modifier.padding(start = MaterialTheme.localDimens.dp32)
+        ) { selectedTab ->
+            tabState = selectedTab
+        }
         VerticalSpacer(height = MaterialTheme.localDimens.dp14)
-        StoriesRow(
-            modifier = Modifier.padding(start = MaterialTheme.localDimens.dp25),
-            lazyListState = lazyListState
-        )
+        if (tabState == HomeScreenTabs.AllStories) {
+            StoriesRow(
+                lazyListState = lazyListState,
+                imgUrls = imgUrls,
+                modifier = Modifier.padding(start = MaterialTheme.localDimens.dp25)
+            )
+        } else {
+            /*todo Favorites*/
+        }
         VerticalSpacer(height = MaterialTheme.localDimens.dp28_5)
         TitleSection(
             author = author,
@@ -117,8 +125,15 @@ fun HomeScreen(
         SummarySection(
             summary = summary,
             numberOfStory = numberOfStory,
-            numberOfViews,
-            numberOfComments
+            numberOfViews = numberOfViews,
+            numberOfComments = numberOfComments,
+            onCommentsClick = {},
+            onListClick = {},
+            modifier = Modifier.padding(
+                top = MaterialTheme.localDimens.dp9,
+                start = MaterialTheme.localDimens.dp25,
+                end = MaterialTheme.localDimens.dp39
+            )
         )
         VerticalSpacer(
             height = MaterialTheme.localDimens.dp50,
@@ -127,88 +142,88 @@ fun HomeScreen(
     }
 }
 
+enum class HomeScreenTabs {
+    AllStories,
+    Favorites
+}
+
 @Composable
 fun HomeScreenTabs(
-    tabState: HomeScreenTabState,
+    storiesSize: Int,
+    favoritesSize: Int,
+    selectedTab: HomeScreenTabs,
     modifier: Modifier = Modifier,
     onTabSelected: (HomeScreenTabs) -> Unit
 ) {
-    val localColors = MaterialTheme.localColors
-    val localDimens = MaterialTheme.localDimens
-
     Row(
         modifier = modifier
     ) {
+        HomeScreenTabItem(
+            title = R.string.all_stories,
+            size = storiesSize,
+            isSelected = selectedTab == HomeScreenTabs.AllStories
+        ) {
+            onTabSelected.invoke(HomeScreenTabs.AllStories)
+        }
+        HorizontalSpacer(width = MaterialTheme.localDimens.dp28)
+        HomeScreenTabItem(
+            title = R.string.favorites_with_size,
+            size = favoritesSize,
+            isSelected = selectedTab == HomeScreenTabs.Favorites
+        ) {
+            onTabSelected.invoke(HomeScreenTabs.Favorites)
+        }
+    }
+}
+
+@Composable
+fun HomeScreenTabItem(
+    @StringRes title: Int,
+    size: Int,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .width(IntrinsicSize.Min)
+            .clickable { onClick.invoke() }
+    ) {
         Text(
-            text = stringResource(id = R.string.all_episodes, tabState.allStories.contentSize),
+            text = stringResource(id = title, size),
             style = MaterialTheme.localTextStyles.homeScreenTabText,
             modifier = Modifier
-                .drawBehind {
-                    if (tabState.allStories.isSelected) {
-                        val cylinderHeight = localDimens.dp4.toPx()
-                        drawRoundRect(
-                            localColors.purple,
-                            size = Size(size.width, cylinderHeight),
-                            cornerRadius = CornerRadius(localDimens.dp3.toPx()),
-                            topLeft = Offset(
-                                localDimens.dp2.toPx(),
-                                size.height - cylinderHeight
-                            )
-                        )
-                    }
-                }
-                .clickable { onTabSelected.invoke(HomeScreenTabs.AllStories) }
+                .requiredWidth(IntrinsicSize.Max)
                 .padding(bottom = MaterialTheme.localDimens.dp6_5)
 
         )
-        HorizontalSpacer(width = MaterialTheme.localDimens.dp28)
-        Text(
-            text = stringResource(
-                id = R.string.favorites_with_size,
-                tabState.favorites.contentSize
-            ),
-            style = MaterialTheme.localTextStyles.homeScreenTabText,
-            modifier = Modifier
-                .drawBehind {
-                    if (tabState.favorites.isSelected) {
-                        drawRoundRect(
-                            localColors.purple,
-                            size = Size(size.width, localDimens.dp4.toPx()),
-                            cornerRadius = CornerRadius(localDimens.dp3.toPx()),
-                            topLeft = Offset(
-                                localDimens.dp2.toPx(),
-                                size.height - localDimens.dp4.toPx()
-                            )
-                        )
-                    }
-                }
-                .clickable { onTabSelected.invoke(HomeScreenTabs.Favorites) }
-                .padding(bottom = MaterialTheme.localDimens.dp6_5)
-        )
+        if (isSelected) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(MaterialTheme.localDimens.dp4)
+                    .clip(MaterialTheme.localShapes.roundedDp3)
+                    .background(MaterialTheme.localColors.purple)
+            )
+        } else {
+            VerticalSpacer(height = MaterialTheme.localDimens.dp4)
+        }
     }
 }
 
 @Composable
 fun StoriesRow(
-    modifier: Modifier = Modifier,
+    imgUrls: List<String>,
     lazyListState: LazyListState,
+    modifier: Modifier = Modifier,
 ) {
     LazyRow(
         state = lazyListState,
         modifier = modifier,
         horizontalArrangement = Arrangement.spacedBy(MaterialTheme.localDimens.dp31),
     ) {
-        item {
-            StoryImage(
-                imgUrl = "https://www.figma.com/file/MYxJBHOTh2JfbmrYbojuxc/image/1d56515ab14098684701024283a07d386bbb94e7?fuid=1097272770330818914",
-                isNew = true
-            ) {}
-        }
-        item {
-            StoryImage(
-                imgUrl = "https://www.figma.com/file/MYxJBHOTh2JfbmrYbojuxc/image/d7ac3769304cdecbbe3a0ff5b327d15512547d7e?fuid=1097272770330818914",
-                isNew = false
-            ) {}
+        items(imgUrls) { url ->
+            StoryImage(imgUrl = url, isNew = true) {}
         }
     }
 }
@@ -275,53 +290,48 @@ fun GenreSection(
     genres: List<String>,
     modifier: Modifier = Modifier
 ) {
-    val firstGenre = genres.first()
-    val secondGenre = genres[1]
-    val localColors = MaterialTheme.localColors
-    val localDimens = MaterialTheme.localDimens
     Row(
         modifier = modifier
     ) {
-        Text(
-            text = firstGenre,
-            style = MaterialTheme.localTextStyles.isStoryNewText,
-            modifier = Modifier
-                .clip(MaterialTheme.localShapes.roundedDp4)
-                .background(MaterialTheme.localColors.purple)
-                .padding(
-                    vertical = MaterialTheme.localDimens.dp6,
-                    horizontal = MaterialTheme.localDimens.dp10
-                )
-        )
+        GenreItem(genres.first(), color = MaterialTheme.localColors.purple)
         HorizontalSpacer(width = MaterialTheme.localDimens.dp10_5)
-        Text(
-            text = secondGenre, style = MaterialTheme.localTextStyles.isStoryNewText,
-            modifier = Modifier
-                .clip(MaterialTheme.localShapes.roundedDp4)
-                .background(MaterialTheme.localColors.pink)
-                .padding(
-                    vertical = MaterialTheme.localDimens.dp6,
-                    horizontal = MaterialTheme.localDimens.dp10
-                )
-        )
+        GenreItem(text = genres[1], color = MaterialTheme.localColors.pink)
         HorizontalSpacer(width = MaterialTheme.localDimens.dp12_5)
-        Text(
-            text = stringResource(id = R.string.episode_size, episodeSize),
-            style = MaterialTheme.localTextStyles.episodeText,
-            modifier = Modifier
-                .align(Alignment.Bottom)
-                .drawBehind {
-                    val roundRectHeight = localDimens.dp4.toPx()
-                    drawRoundRect(
-                        color = localColors.white_alpha08,
-                        size = Size(width = size.width, height = roundRectHeight),
-                        cornerRadius = CornerRadius(localDimens.dp3.toPx()),
-                        topLeft = Offset(0f, size.height - roundRectHeight)
-                    )
-                }
-                .padding(bottom = 0.5.dp)
-        )
+        Column(
+            modifier = Modifier.width(IntrinsicSize.Min)
+        ) {
+            Text(
+                text = stringResource(id = R.string.episode_size, episodeSize),
+                style = MaterialTheme.localTextStyles.episodeText,
+                modifier = Modifier.requiredWidth(IntrinsicSize.Max)
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(MaterialTheme.localDimens.dp4)
+                    .clip(MaterialTheme.localShapes.roundedDp3)
+                    .background(MaterialTheme.localColors.white_alpha08)
+            )
+        }
     }
+}
+
+@Composable
+fun GenreItem(
+    text: String,
+    color: Color
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.localTextStyles.isStoryNewText,
+        modifier = Modifier
+            .clip(MaterialTheme.localShapes.roundedDp4)
+            .background(color)
+            .padding(
+                vertical = MaterialTheme.localDimens.dp6,
+                horizontal = MaterialTheme.localDimens.dp10
+            )
+    )
 }
 
 @Composable
@@ -330,16 +340,15 @@ fun SummarySection(
     numberOfStory: Int,
     numberOfViews: Int,
     numberOfComments: Int,
+    modifier: Modifier = Modifier,
+    onCommentsClick: () -> Unit,
+    onListClick: () -> Unit
 ) {
     val localDimens = MaterialTheme.localDimens
+
     ConstraintLayout(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
-            .padding(
-                top = MaterialTheme.localDimens.dp9,
-                start = MaterialTheme.localDimens.dp25,
-                end = MaterialTheme.localDimens.dp39
-            )
     ) {
         val (summaryText, views, comments, info, listIcon) = createRefs()
 
@@ -373,11 +382,13 @@ fun SummarySection(
         }
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.constrainAs(comments) {
-                start.linkTo(views.start)
-                end.linkTo(views.end)
-                bottom.linkTo(summaryText.bottom)
-            }
+            modifier = Modifier
+                .clickable { onCommentsClick.invoke() }
+                .constrainAs(comments) {
+                    start.linkTo(views.start)
+                    end.linkTo(views.end)
+                    bottom.linkTo(summaryText.bottom)
+                }
         ) {
             SimpleIcon(
                 iconResId = R.drawable.ic_comment,
@@ -394,18 +405,20 @@ fun SummarySection(
             modifier = Modifier.constrainAs(info) {
                 top.linkTo(summaryText.bottom, localDimens.dp36)
                 start.linkTo(parent.start)
-                end.linkTo(listIcon.start)
+                end.linkTo(listIcon.start, localDimens.dp26)
                 width = Dimension.fillToConstraints
             }
         )
         SimpleIcon(
             iconResId = R.drawable.ic_list,
-            modifier = Modifier.constrainAs(listIcon) {
-                top.linkTo(info.top)
-                bottom.linkTo(info.bottom)
-                start.linkTo(comments.start)
-                end.linkTo(comments.end)
-            }
+            modifier = Modifier
+                .clickable { onListClick.invoke() }
+                .constrainAs(listIcon) {
+                    top.linkTo(info.top)
+                    bottom.linkTo(info.bottom)
+                    start.linkTo(comments.start)
+                    end.linkTo(comments.end)
+                }
         )
     }
 }
@@ -426,21 +439,10 @@ fun HomeScreenPreview() {
         numberOfComments = 142,
         numberOfFavorites = 5,
         episodeSize = 35,
-        summary = "Kim olduğunu sorguladıkça dünyasının sahtelikten İbaret olduğunu anlamaya başlayan Işıl Özsoydan, öğrendiği gerçekleri..."
+        summary = LoremIpsum(16).values.joinToString(),
+        imgUrls = listOf(
+            "https://www.figma.com/file/MYxJBHOTh2JfbmrYbojuxc/image/1d56515ab14098684701024283a07d386bbb94e7?fuid=1097272770330818914",
+            "https://www.figma.com/file/MYxJBHOTh2JfbmrYbojuxc/image/d7ac3769304cdecbbe3a0ff5b327d15512547d7e?fuid=1097272770330818914"
+        )
     )
-}
-
-data class HomeScreenTabState(
-    val allStories: HomeScreenTabInfo,
-    val favorites: HomeScreenTabInfo
-)
-
-data class HomeScreenTabInfo(
-    val contentSize: Int,
-    val isSelected: Boolean
-)
-
-enum class HomeScreenTabs {
-    AllStories,
-    Favorites
 }
