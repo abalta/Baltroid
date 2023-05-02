9b0e315e63e614ddbb34624f584cf95f58815404
tunahanbozkurt <t.bozkurt5775@gmail.com>
2023-05-02 04:12:31 +0300

    PlaceMarksScreen added.

--- app/src/main/java/com/baltroid/presentation/screens/menu/PlaceMarksScreen.kt
+++ app/src/main/java/com/baltroid/presentation/screens/menu/PlaceMarksScreen.kt
@@ -1,16 +1,18 @@
 package com.baltroid.presentation.screens.menu
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
@@ -24,19 +26,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ClipOp
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import com.baltroid.apps.R
 import com.baltroid.presentation.common.CroppedImage
-import com.baltroid.presentation.common.HorizontalSpacer
 import com.baltroid.presentation.common.SimpleIcon
 import com.baltroid.presentation.common.VerticalSpacer
+import com.baltroid.presentation.screens.menu.components.MenuBar
 import com.baltroid.ui.theme.localColors
 import com.baltroid.ui.theme.localDimens
 import com.baltroid.ui.theme.localShapes
@@ -50,14 +49,14 @@ fun PlaceMarksScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black)
+            .background(MaterialTheme.localColors.black)
             .verticalScroll(scrollState)
+            .navigationBarsPadding()
     ) {
         VerticalSpacer(height = MaterialTheme.localDimens.dp36)
         MenuBar(
             modifier = Modifier.fillMaxWidth(),
             title = stringResource(id = R.string.place_marks),
-            isIconVisible = true,
             iconResId = R.drawable.ic_banner_filled
         )
         VerticalSpacer(height = MaterialTheme.localDimens.dp16)
@@ -95,11 +94,11 @@ fun StoryItemList(
         modifier = modifier,
         horizontalArrangement = Arrangement.spacedBy(MaterialTheme.localDimens.dp29)
     ) {
-        item { StoryItem() }
-        item { StoryItem() }
-        item { StoryItem() }
-        item { StoryItem() }
-        item { StoryItem() }
+        item { StoryItem("KİMSE GERÇEK DEĞİL", 25) }
+        item { StoryItem("KİMSE GERÇEK DEĞİL", 25) }
+        item { StoryItem("KİMSE GERÇEK DEĞİL", 25) }
+        item { StoryItem("KİMSE GERÇEK DEĞİL", 25) }
+        item { StoryItem("KİMSE GERÇEK DEĞİL", 25) }
     }
 }
 
@@ -111,16 +110,19 @@ fun MarkItemList(
         modifier = modifier,
         horizontalArrangement = Arrangement.spacedBy(MaterialTheme.localDimens.dp29)
     ) {
-        item { MarkItem() }
-        item { MarkItem() }
-        item { MarkItem() }
-        item { MarkItem() }
-        item { MarkItem() }
+        item { MarkItem("KİMSE GERÇEK DEĞİL", 25) }
+        item { MarkItem("KİMSE GERÇEK DEĞİL", 25) }
+        item { MarkItem("KİMSE GERÇEK DEĞİL", 25) }
+        item { MarkItem("KİMSE GERÇEK DEĞİL", 25) }
+        item { MarkItem("KİMSE GERÇEK DEĞİL", 25) }
     }
 }
 
 @Composable
-fun StoryItem() {
+fun StoryItem(
+    title: String,
+    episodeNumber: Int
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.width(IntrinsicSize.Min)
@@ -135,17 +137,16 @@ fun StoryItem() {
                 .clip(MaterialTheme.localShapes.roundedDp18)
         )
         VerticalSpacer(height = MaterialTheme.localDimens.dp13)
-        Text(text = "KİMSE GERÇEK DEĞİL", style = MaterialTheme.localTextStyles.storyItemTitle)
+        Text(text = title, style = MaterialTheme.localTextStyles.storyItemTitle)
         Column(
             modifier = Modifier.width(IntrinsicSize.Min)
         ) {
             Row {
                 Text(
-                    text = stringResource(id = R.string.episode),
-                    style = MaterialTheme.localTextStyles.episodeSelectedText
+                    text = stringResource(id = R.string.episode_size, episodeNumber),
+                    style = MaterialTheme.localTextStyles.episodeSelectedText,
+                    modifier = Modifier.requiredWidth(IntrinsicSize.Max)
                 )
-                HorizontalSpacer(width = MaterialTheme.localDimens.dp4)
-                Text(text = "25", style = MaterialTheme.localTextStyles.episodeSelectedText)
             }
             VerticalSpacer(height = MaterialTheme.localDimens.dp5)
             EpisodeBanner(modifier = Modifier.fillMaxWidth())
@@ -158,20 +159,26 @@ fun EpisodeBanner(
     modifier: Modifier
 ) {
     val localColor = MaterialTheme.localColors
-    Canvas(modifier = modifier.height(MaterialTheme.localDimens.dp17), onDraw = {
-        val path = Path().apply {
-            moveTo(size.width / 2f, size.height / 3f)
-            lineTo(0f, size.height)
-            lineTo(size.width, size.height)
+    Canvas(
+        modifier = modifier.height(MaterialTheme.localDimens.dp17),
+        onDraw = {
+            val path = Path().apply {
+                moveTo(size.width / 2f, size.height / 3f)
+                lineTo(0f, size.height)
+                lineTo(size.width, size.height)
+            }
+            clipPath(path, ClipOp.Difference) {
+                drawRect(localColor.purple_dark)
+            }
         }
-        clipPath(path, ClipOp.Difference) {
-            drawRect(localColor.purple)
-        }
-    })
+    )
 }
 
 @Composable
-fun MarkItem() {
+fun MarkItem(
+    title: String,
+    episodeNumber: Int
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.width(IntrinsicSize.Min)
@@ -186,17 +193,16 @@ fun MarkItem() {
                 .clip(MaterialTheme.localShapes.roundedDp18)
         )
         VerticalSpacer(height = MaterialTheme.localDimens.dp13)
-        Text(text = "KİMSE GERÇEK DEĞİL", style = MaterialTheme.localTextStyles.storyItemTitle)
+        Text(text = title, style = MaterialTheme.localTextStyles.storyItemTitle)
         Column(
             modifier = Modifier.width(IntrinsicSize.Min)
         ) {
             Row {
                 Text(
-                    text = stringResource(id = R.string.episode),
-                    style = MaterialTheme.localTextStyles.episodeSelectedText
+                    text = stringResource(id = R.string.episode_size, episodeNumber),
+                    style = MaterialTheme.localTextStyles.episodeSelectedText,
+                    modifier = Modifier.requiredWidth(IntrinsicSize.Max)
                 )
-                HorizontalSpacer(width = MaterialTheme.localDimens.dp4)
-                Text(text = "25", style = MaterialTheme.localTextStyles.episodeSelectedText)
             }
             VerticalSpacer(height = MaterialTheme.localDimens.dp7)
             YellowBar(modifier = Modifier.fillMaxWidth())
@@ -217,45 +223,32 @@ fun YellowBar(
 }
 
 @Composable
-fun MenuBar(
-    title: String,
-    @DrawableRes iconResId: Int,
-    isIconVisible: Boolean,
+fun IconlessMenuBar(
     modifier: Modifier = Modifier
 ) {
-    val dp20 = MaterialTheme.localDimens.dp20
+    Column(
+        modifier = modifier
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "ZEYNEP SEY",
+                style = MaterialTheme.localTextStyles.menuBarTitle,
+                modifier = Modifier.align(
+                    Alignment.Center
+                )
+            )
+            SimpleIcon(
+                iconResId = R.drawable.ic_close,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
 
-    ConstraintLayout(modifier = modifier) {
-
-        val (banner, text, close, divider) = createRefs()
-        createHorizontalChain(banner, text, close)
-
-        if (isIconVisible) {
-            SimpleIcon(iconResId = iconResId, modifier = Modifier.constrainAs(banner) {
-                top.linkTo(text.top)
-                bottom.linkTo(text.bottom)
-            })
         }
-        Text(
-            text = title,
-            style = MaterialTheme.localTextStyles.menuBarTitle,
-            modifier = Modifier.constrainAs(text) {
-                top.linkTo(parent.top)
-            }
-        )
-        SimpleIcon(iconResId = R.drawable.ic_close, modifier = Modifier.constrainAs(close) {
-            top.linkTo(text.top)
-            bottom.linkTo(text.bottom)
-        })
+        VerticalSpacer(height = MaterialTheme.localDimens.dp20)
         Divider(
             thickness = MaterialTheme.localDimens.dp0_5,
-            color = MaterialTheme.localColors.white_alpha06,
-            modifier = Modifier.constrainAs(divider) {
-                start.linkTo(banner.start)
-                end.linkTo(close.end)
-                top.linkTo(text.bottom, dp20)
-                width = Dimension.fillToConstraints
-            }
+            color = MaterialTheme.localColors.white_alpha06
         )
     }
 }
