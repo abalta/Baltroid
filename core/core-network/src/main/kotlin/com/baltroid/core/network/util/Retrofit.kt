9732a5338205d0b92f181a1be481718c22ad2181
abdullah <abdullahbalta90@gmail.com>
2023-05-11 21:39:28 +0300

    Add HttpLogging interceptor.

--- core/core-network/src/main/kotlin/com/baltroid/core/network/util/Retrofit.kt
+++ core/core-network/src/main/kotlin/com/baltroid/core/network/util/Retrofit.kt
@@ -3,12 +3,24 @@ package com.baltroid.core.network.util
 import com.baltroid.core.network.retrofit.ResultAdapterFactory
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 internal fun retrofit(
     json: Json = defaultJson
 ): Retrofit = Retrofit.Builder()
     .baseUrl(Constants.API_URL)
+    .client(loggingOkHttpClient())
     .addConverterFactory(json.asConverterFactory(MIMETYPE_JSON))
     .addCallAdapterFactory(ResultAdapterFactory())
     .build()
+
+private fun loggingOkHttpClient(): OkHttpClient {
+    val logging = HttpLoggingInterceptor()
+    logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+    return OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+}
+
